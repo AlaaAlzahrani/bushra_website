e761d336e9ebfc91d47bda3872cfde91627e2da2
AlaaAlzahrani <alzahrani.alaaa@gmail.com>
2024-09-08 00:10:38 +0300

    lazy next config settings

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 }
 
 module.exports = nextConfig
